43930254b7d34dd5b24e6ea88f9960cf8e1d77a7
cewing <cris@crisewing.com>
2012-05-08 22:35:17 -0700

    * add package trove classifiers  * add dependencies for package  * add entry point for contenttype sub-template

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.1a'
 
@@ -21,8 +20,22 @@ setup(name='templer.plone.localcommands',
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        ],
+          "Development Status :: 3 - Alpha",
+          "Environment :: Environment :: Console",
+          "Framework :: Zope2",
+          "Framework :: Zope3",
+          "Framework :: Plone",
+          "Framework :: Buildout",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          'Programming Language :: Python :: 2.4',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+          "Topic :: Software Development :: Code Generators",
+      ],
       keywords='zope plone python',
       author='Cris Ewing',
       author_email='cris@crisewing.com',
@@ -36,8 +49,12 @@ setup(name='templer.plone.localcommands',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
+          'templer.localcommands',
+          'templer.plone',
       ],
       entry_points="""
       # -*- Entry points: -*-
+      [templer.templer_sub_template]
+      contenttype = templer.plone.localcommands.archetype:ContentType
       """,
       )
